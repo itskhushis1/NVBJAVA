51dcac25923227afcacafc5ca66961870c3cd031
Khushi Sharma <70655479+itskhushis1@users.noreply.github.com>
2022-01-26 21:06:01 +0530

    Create tut1.java

---
+++ tut1.java
@@ -0,0 +1,51 @@
+/* NAME IN UPPERCASE */
+
+package jan18;
+
+public class tutorial1 {
+
+	public static void main(String[] args) {
+		System.out.println("Khushi Sharma");
+	}
+}
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  OUTPUT :
+  Khushi Sharma
+  
+  
+  
+
+
+/* ADDRESS */
+
+package jan18;
+
+public class Address {
+
+	public static void main(String[] args) {
+		        System.out.println("Miss. Khushi,");
+		        System.out.println("Door No - 202/2,");
+		        System.out.println("Street No. - 23,");
+		        System.out.println("City - HYDERABAD,");
+		        System.out.println("Pin - 509301.");
+		   }
+	}
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+OUTPUT:
+Miss. Khushi,
+Door No - 202/2,
+Street No. - 23,
+City - HYDERABAD,
+Pin - 509301.
+  
+  
+  
+
+
+
+  
+  
+  
+  
