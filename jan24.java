8ce150d7b37f257395aabcd61c8709151a177949
Khushi Sharma <70655479+itskhushis1@users.noreply.github.com>
2022-01-27 01:27:23 +0530

    Create jan24.java

---
+++ jan24.java
@@ -0,0 +1,166 @@
+
+/* STRING */
+
+package jan24;
+
+public class string {
+
+	public static void main(String[] args) {
+		String s1;
+		s1 = new String("VIT Bhopal University");
+		String s2 = "Welcome to Java Programming";
+
+		System.out.println(s1);
+		System.out.println("s2 contains " + s2);
+		int len = s1.length();
+		for(int i=0; i<len;i++)
+		System.out.println(s1.charAt(i));
+		if(s1.equals("VIT Bhopal University"))
+		System.out.println("Both are equal");
+		else
+		System.out.println("Both are differnt");
+		System.out.println("Apple".compareTo("Ape"));
+		if ("Apple".startsWith("Ap"))
+		System.out.println("Yes");
+		if (s1.endsWith("University"))
+		System.out.println("Yes");
+		System.out.println(s1.indexOf("Big"));
+	}
+
+}
+
+
+
+OUTPUT:
+
+VIT Bhopal University
+s2 contains Welcome to Java Programming
+V
+I
+T
+ 
+B
+h
+o
+p
+a
+l
+ 
+U
+n
+i
+v
+e
+r
+s
+i
+t
+y
+Both are equal
+11
+Yes
+Yes
+-1
+  
+  
+------------------------------------------------------------------------------------------------------------------------------------------------------------------  
+  
+  
+/* EXTENSION */
+  
+
+package jan24;
+
+import java.util.Scanner;
+
+public class extension {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the file name : ");
+		String s= sc.nextLine();
+		int index = s.lastIndexOf('.');
+		if(index > 0) {
+		String extension = s.substring(index + 1);
+		System.out.println("File extension is : " + extension);
+		  }
+		}
+}
+
+
+OUTPUT :
+
+Enter the file name : 
+word.sft
+File extension is : sft
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* CITIES*/
+
+
+package jan24;
+
+public class City {
+
+	public static void main(String[] args) {
+		String city = "Bhopal";
+		String cityUppercase = city.toUpperCase(); // toLowerCase() is also available
+		System.out.println("city = " + city);
+		System.out.println("cityUppercase = "+ cityUppercase);
+		city = "Chennai";
+		city.replace('e', 'E');
+		System.out.println("city = " + city);
+		System.out.println(city.replace('e', 'E'));
+	}
+
+}
+
+
+OUTPUY:
+city = Bhopal
+cityUppercase = BHOPAL
+city = Chennai
+ChEnnai
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* STRING DEMO */
+
+package jan24;
+
+public class Stringdemo {
+
+	public static void main(String[] args) {
+		String str1 = "Hello";
+		String str2 = "Hello";
+		if(str1 == str2)
+		System.out.println("Both are same");
+		else
+		System.out.println("Both are different");
+		String str3 = new String("Hello");
+		if(str1 == str3)
+		System.out.println("Both are same");
+		else
+		System.out.println("Both are different");
+		if(str1.equals(str3))
+		System.out.println("Both are equal");
+		else
+		System.out.println("Both are different");
+
+	}
+
+}
+
+
+OUTPUT:
+
+Both are same
+Both are different
+Both are equal
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
